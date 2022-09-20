780db6ba4f791f537c34e90fbe561a53d2f5c63d
Mahijith <menon.mahijith@gmail.com>
2022-09-20 13:34:30 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar.jsx';
+import Feed from './Feed.jsx';
+import VideoDetail from './VideoDetail.jsx';
+import ChannelDetail from './ChannelDetail.jsx';
+import SearchFeed from './SearchFeed.jsx';
+import SearchBar from './SearchBar.jsx';
+import SideBar from './Sidebar.jsx';
+import Videos from './Videos.jsx';
+import VideoCard from './VideoCard.jsx';
+import ChannelCard from './ChannelCard.jsx';
+
+export{Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed,SearchBar, SideBar,Videos,VideoCard,ChannelCard}
